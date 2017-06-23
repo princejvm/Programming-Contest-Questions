30ffb5e44f9e5a976474ed815cd2a079ae6732aa
princejvm <sksinha.c@gmail.com>
2017-06-23 18:57:17 +0530

    Create game_of_coins.cpp
    Sprague–Grundy theorem

---
+++ Hackerearth/CodeRingIV/game_of_coins.cpp
@@ -0,0 +1,43 @@
+/*Any fool can write code that a computer can understand. Good programmers write code that humans can understand.*/
+//Que link-> https://www.hackerearth.com/problem/algorithm/game-of-coins-264/
+//Author-->Prince Kumar 
+//Handle-->princejvm
+#include<bits/stdc++.h>
+using namespace std;
+int LP[10000007];
+vector<int>prime;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k,p;
+        cin>>n>>k>>p;
+        if(n%2!=0)
+        {
+            if(p==1&&k!=1)
+            {cout<<"BRAN"<<"\n";continue;}
+            if(p==1&&k==1)
+            {cout<<"ARYA"<<"\n";continue;}
+            int c=0;
+            for(int i=2;i<=sqrt(k);i++)
+            {
+                int l=k/i; 
+                if((k%i==0)&&(i>=p||l>=p))
+                {
+                    c=1;
+                    break;
+                }
+            }
+            if(c>0)
+            cout<<"BRAN"<<"\n";
+            else cout<<"ARYA"<<"\n";
+ 
+        }
+        else cout<<"ARYA"<<"\n";
+    }
+    return 0;
+}
